ed83274d3e40d740c81db3acbeeab05c3b76887e
Mark Abella <mark.abella@gmail.com>
2017-06-23 02:30:46 +0000

    Checkpoint 5 Menu complete

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -4,7 +4,7 @@
    
    describe "attributes" do
      let(:entry) { Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com') }
-     # #3
+
      it "responds to name" do
        expect(entry).to respond_to(:name)
      end
@@ -30,12 +30,11 @@
      end
    end
    
-   # #5
    describe "#to_s" do
      it "prints an entry as a string" do
        entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
        expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
-   # #6
+
        expect(entry.to_s).to eq(expected_string)
      end
    end
